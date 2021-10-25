e8c165da6858408a3761fbb2ed76f0a84d3d60c8
Charlene Bendaña <bendanacharlene@gmail.com>
2021-10-25 16:28:52 -0700

    Added body index , button and for outputs

--- Desktop/Lesson-2.DOM-Interaction-activities/Events Binding/js/app.js
+++ Desktop/Lesson-2.DOM-Interaction-activities/Events Binding/js/app.js
@@ -5,3 +5,17 @@ new Vue({
         output1: '',
         output2: ''
     },
+    methods:{
+        ShowAlert()
+        {
+            alert('The Alert Was Pop-up');
+        },
+        setoutput1(events){
+            this.output1=event.target.value
+        },
+        setoutput2(events){
+            this.output2=event.target.value
+        },
+
+    },
+})
